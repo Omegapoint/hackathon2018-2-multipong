f0bea5df0585ef7a812f2ef45acc5d478918ded0
Daniel Deogun <daniel@deogun.se>
2018-10-12 20:11:46 +0200

    refactor

--- server/graphicLogic/ballObject.js
+++ server/graphicLogic/ballObject.js
@@ -1,16 +1,4 @@
 module.exports = (ball, dt) => {
-    function isOutOfBounds(aBall) {
-        return false
-    }
-
-    function collidedWithPad() {
-        return false
-    }
-
-    function hej() {
-        return false
-    }
-
     ball.y += Math.cos(ball.angle) * ball.v * dt;
     ball.x += Math.sin(ball.angle) * ball.v * dt;
 
