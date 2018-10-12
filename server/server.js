447cb2db52ba32784530eb77a9e0d1a355227c9a
Daniel Deogun <daniel@deogun.se>
2018-10-12 14:48:44 +0200

    milestone 1

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/register', function(req, res) {
+   console.log(req.body);
+   res.status(418).send();
 });
 
 http.listen(3000, function(){
